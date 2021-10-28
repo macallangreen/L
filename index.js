e949135628a1de0d6fe572d17f1a8eefd35bd276
Macallan Green <macallangreen@MY-computer-NOT-YOURS.local>
2021-10-27 20:26:47 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,192 @@
+console.log(`
+
+  ifDone() get large cats
+  ifDone
+  ifDone you mother
+  fat man cute () haha career your dumb
+
+  HAHAHAHA imagine not coding in 2021 lamo get  life kid carreer.com
+  im coding rn l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l l 
+
+
+
+
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+
+
+
+
+
+  l l l l l l l l 
+
+
+
+
+
+
+
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  v
+
+
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+
+
+
+  l l l l l l l l l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+
+
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  v
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  l l l l l l l l 
+  
+`)
